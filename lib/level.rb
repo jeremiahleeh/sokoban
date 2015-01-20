b6bb76cf1d562dc7beac20d88036194b77e6bd2f
Jonan Scheffler <jonanscheffler@gmail.com>
2015-01-19 21:44:32 -0800

    WIP

--- lib/level.rb
+++ lib/level.rb
@@ -8,8 +8,28 @@ class Level
   	rows = new_abyss.split('/n')
 
   	rows.each do |row|
-      @abyss.push row.split
+      @abyss.push row.split(//)
   	end
   end
 
+  def move(direction)
+  	case direction
+  	when :right
+  	  # move to the right!
+  	end
+  end
+
+  def person_location
+  	width = @abyss.first.length
+  	simple_location = @abyss.flatten.index('O')
+  	x = simple_location % width
+
+  	y = 0
+  	while simple_location > width
+  	  simple_location -= width
+  	  y += 1
+  	end
+
+  	[x, y]
+  end
 end
--- test/level_test.rb
+++ test/level_test.rb
@@ -5,10 +5,26 @@ class LevelTest < Minitest::Test
   describe "#read_level" do
     before do
       @level = Level.new
+      @level.read_abyss(1)
     end
 
     it "should include a wall" do
       @level.read_abyss(1).to_s.must_include "---"
+    end
+  end
+
+  describe "#move" do
+  	before do
+      @level = Level.new
+      @level.read_abyss(1)
+    end
+
+    it "should move the person to the right" do
+      old_x, _ = @level.person_location # [x, y]
+      @level.move(:right)
+      new_x, _ = @level.person_location
+
+      new_x.must_equal old_x + 1
     end
   end
 end
